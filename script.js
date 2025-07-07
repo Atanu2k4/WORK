63ae17b82c65e340943b02851d8faaf201dfde60
Atanu2k4 <atanu.saha2004@gmail.com>
2025-07-07 20:36:33 +0530

    Locomotive intricated

--- script.js
+++ script.js
@@ -1,28 +1,49 @@
-const tl = gsap.timeline();
+function loading() {
+  const tl = gsap.timeline();
 
-tl.to(".loader .yellow1", {
-  top: "-100%",
-  delay: 0.5,
-  duration: 0.7,
-  ease: "expo.out",
-});
+  tl.to(".loader .yellow1", {
+    top: "-100%",
+    delay: 0.5,
+    duration: 0.7,
+    ease: "expo.out",
+  });
 
-tl.from(
-  ".loader .yellow2",
-  {
+  tl.from(".loader .yellow2", {
     top: "100%",
     delay: 0.9,
     duration: 0.7,
     ease: "expo.out",
-  },
-);
+  });
 
-tl.to(".loader", {
-  display: "none",
-},"anim");
+  tl.to(
+    ".loader",
+    {
+      opacity: 0,
+    },
+    "anim"
+  );
 
-tl.to(".loader h1", {
-  delay: 0.6,
-  duration: 0.7,
-  color: "black",
-},"anim");
+  tl.to(
+    ".loader",
+    {
+      display:"none",
+    },
+    "anim"
+  );
+
+  tl.to(
+    ".loader h1",
+    {
+      delay: 0.6,
+      duration: 0.7,
+      color: "black",
+    },
+    "anim"
+  );
+}
+loading();
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true,
+});
